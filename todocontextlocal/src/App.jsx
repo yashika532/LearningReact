763e990a0b78f0060fe03a32230a75de8ddfcd1c
yashika532 <jainyashika532@gmail.com>
2024-07-02 01:01:28 +0530

    to do

--- todocontextlocal/src/App.jsx
+++ todocontextlocal/src/App.jsx
@@ -37,7 +37,7 @@ function App() {
 
   return (
     <Todoprovider value={{Todos,addTodo,updateTodo,deleteTodo,toggleComplete}}>
-    <div className=" bg-gradient-to-r from-cyan-500 to-blue-500 min-h-screen py-8">
+    <div  className=" bg-cyan-950 min-h-screen py-8">
                 <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
                     <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
                     <div className="mb-4">
--- todocontextlocal/src/components/TodoForm.jsx
+++ todocontextlocal/src/components/TodoForm.jsx
@@ -22,7 +22,7 @@ function TodoForm() {
               value={todo}
               onChange={(e)=>setTodo(e.target.value)}
           />
-          <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
+          <button type="submit" className="rounded-r-lg px-3 py-1 bg-cyan-500 hover:bg-sky-700 text-white shrink-0">
               Add
           </button>
       </form>
